a113495da7352e5eace0d87e8a90ca9fc00b71c0
Sangwoo97 <aa6618@naver.com>
2023-10-13 17:43:38 +0900

    feat: todo list 기능 완성

--- client/src/App.js
+++ client/src/App.js
@@ -16,24 +16,36 @@ function App() {
     getTodo();
   }, []);
 
-  const addItem = (newItem) => {
-    newItem.id = todoItems.length + 1;
-    newItem.done = false;
+  const addItem = async (newItem) => {
+    // newItem.id = todoItems.length + 1;
+    // newItem.done = false;
 
-    // todoItems 배열에 newItem을 추가
-    setTodoItems([...todoItems, newItem]);
+    // // todoItems 배열에 newItem을 추가
+    // setTodoItems([...todoItems, newItem]);
+    const res = await axios.post(`${process.env.REACT_APP_DB_HOST}/todo`, newItem);
+    setTodoItems([...todoItems, res.data]);
   };
 
-  const deleteItem = (item) => {
-    const temp = todoItems.filter((e) => e.id !== item.id);
-    setTodoItems(temp);
+  const deleteItem = async (item) => {
+    // const temp = todoItems.filter((e) => e.id !== item.id);
+    // setTodoItems(temp.sort((a, b) => b.id - a.id));
+    const res = await axios.delete(`${process.env.REACT_APP_DB_HOST}/todo/${item.id}`);
+    setTodoItems(todoItems.filter((e) => e.id !== item.id));
+  };
+
+  const updateItem = async (item) => {
+    const res = await axios.patch(`${process.env.REACT_APP_DB_HOST}/todo/${item.id}`, {
+      title: item.title,
+      done: item.done,
+    });
   };
 
   return (
     <div className='App'>
       <AddTodo addItem={addItem} />
+      {todoItems.length}
       {todoItems.map((e, i) => {
-        return <Todo items={e} key={e.id} deleteItem={deleteItem} />;
+        return <Todo items={e} key={e.id} deleteItem={deleteItem} updateItem={updateItem} />;
       })}
     </div>
   );
--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Todo({ items, deleteItem }) {
+export default function Todo({ items, deleteItem, updateItem }) {
+  const [readOnly, setReadOnly] = useState(false);
+  const [todoItem, setTodoItem] = useState(items);
+  const { id, done, title } = todoItem;
+
   const onDeleteButtonClick = () => {
     deleteItem(items);
   };
+
+  const editKeyEventListener = (e) => {
+    if (e.key === 'Enter') {
+      setReadOnly(true);
+      updateItem(todoItem);
+    }
+  };
+
+  const editKeyHandler = (e) => {
+    const { title, ...rest } = todoItem;
+    setTodoItem({
+      title: e.target.value,
+      ...rest,
+    });
+  };
+
+  const checkboxEventListener = (e) => {
+    const { done, ...rest } = todoItem;
+    const updatedItem = {
+      done: e.target.checked,
+      ...rest,
+    };
+    setTodoItem(updatedItem);
+    updateItem(updatedItem);
+  };
+
   return (
     <div>
-      <input type='checkbox' name='todo0' id='todo0' checked={items.done} />
-      <label htmlFor={`todo${items.id}`}>{items.title}</label>
+      <input type='checkbox' name={`todo${id}`} id={`todo${id}`} checked={done} onChange={checkboxEventListener} />
+      <input
+        type='text'
+        readOnly={readOnly}
+        onClick={() => setReadOnly(false)}
+        value={title}
+        onChange={editKeyHandler}
+        onKeyDown={editKeyEventListener}
+      />
       <button onClick={onDeleteButtonClick}>DELETE</button>
     </div>
   );
